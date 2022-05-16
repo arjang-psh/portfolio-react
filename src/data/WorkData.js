64768c7fa6b18b8329dbcb7e6a103f2e98d326d6
Arjang Pezeshki <arjangpezeshki@Arjangs-MacBook-Pro.local>
2022-05-16 16:33:39 -0700

    first portfolio added to projects page

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -15,20 +15,11 @@ export const Work = [
         tags: ["react", "styled-components", "framer-motion", "react-router", "react-tsparticles"],
 
         demo: "http://arjangdev.site/",
-        github: "https://github.com/arjang-psh/portfolio"
+        github: "https://github.com/arjang-git addpsh/portfolio"
 
     },
     {
         id: 4,
-        name: "LinkedIn clone",
-        description: "A fake LinkedIn clone using React JS Styled components and have Real-time database with Firebase's Firestore.",
-        tags: ["react", "firebase", "redux"],
-
-        demo: "https://netflix-clone-dummy.netlify.app/",
-        github: "https://github.com/arjang-psh/linkedin-react"
-    },
-    {
-        id: 3,
         name: "Netflix clone",
         description: "A fake Netflix clone useing react and real API to fetch updated data.",
         tags: ["react", "styledComponents", "react-router", "netlify"],
@@ -37,12 +28,20 @@ export const Work = [
         github: "https://github.com/arjang-psh/netflix-clone"
 
     }, {
+        id: 3,
+        name: "LinkedIn clone",
+        description: "A fake LinkedIn clone using React JS Styled components and have Real-time database with Firebase's Firestore.",
+        tags: ["react", "firebase", "redux"],
+
+        demo: "https://netflix-clone-dummy.netlify.app/",
+        github: "https://github.com/arjang-psh/linkedin-react"
+    }, {
         id: 5,
         name: "First Portfoli website",
         description: "A dynamic and fully responsive frontend templates with HTML/CSS/JavaScript/Bootstrap. Building the website’s backend using Django and PostgreSQL.",
         tags: ["html", "css", "javascript", "bootsrap", "django", "postgresql"],
 
-        demo: "https://github.com/arjang-psh/portfolio",
+        demo: "https://arjang-first-portfolio.netlify.app/",
         github: "https://github.com/arjang-psh/portfolio"
 
     },
