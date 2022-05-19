ee4904c8164eeb111f281725be78abcc94b00f50
Arjang Pezeshki <arjangpezeshki@Arjangs-MacBook-Pro.local>
2022-05-19 01:18:02 -0700

    resume changed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
-import Me from '../assets/Images/profile1-img.png'
+import Me from '../assets/Images/profile-img.png'
 
 
 const Box = styled(motion.div)`
@@ -45,7 +45,7 @@ display: flex;
     left: 50%;
     transform: translate(-50%,0%);
     width: 100%;
-    height: 100%;
+    height: auto;
     /* max-height: max-content; */
 }
 `
--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,7 +1,7 @@
 export const Work = [
     {
         id: 1,
-        name: "Social Media Growth Service Website",
+        name: "Social media growth service Website",
         description: "Developed the project from scratch, designed website wireframe, layout, UI, and user flow model.",
         tags: ["html", "css", "javaScript", "bootstrap", "django"],
 
@@ -11,40 +11,30 @@ export const Work = [
     {
         id: 2,
         name: "Personal Portfolio Website",
-        description: "Build a Stunning Portfolio with React JS using Styled-Components and framer-motion for awesome Animations.",
+        description: "Build a stunning Portfolio with React Js using Styled-components and framer-motion for awesome Animations.",
         tags: ["react", "styled-components", "framer-motion", "react-router", "react-tsparticles"],
 
         demo: "http://arjangdev.site/",
-        github: "https://github.com/arjang-git/react-portfolio"
+        github: "https://github.com/arjang-psh/portfolio-react"
 
+    }, {
+        id: 3,
+        name: "LinkedIn Clone",
+        description: "Create a full stack, LinkedIn clone using React, Firebase, Redux, and Styled Component.",
+        tags: ["react", "styled-components", "firebase", "redux", "google-authentication", "netlify"],
+
+        demo: "https://arjang-linkedin-clone.netlify.app/",
+        github: "https://github.com/arjang-psh/linkedin-react"
     },
     {
-        id: 3,
-        name: "Netflix clone",
-        description: "A fake Netflix clone useing react and real API to fetch updated data from TMBD api.",
-        tags: ["react", "styledComponents", "react-router", "netlify"],
+        id: 4,
+        name: "Netflix Clone Website",
+        description: "A Netflix clone useing React Js and real API functioning to fetch updated data from TMBD api.",
+        tags: ["react", "styled-components", "react-router", "netlify"],
 
         demo: "https://arjang-netflix-clone.netlify.app/",
         github: "https://github.com/arjang-psh/netflix-clone"
 
-    }, {
-        id: 4,
-        name: "LinkedIn clone",
-        description: "A fake LinkedIn clone using React JS Styled components and have Real-time database with Firebase's Firestore.",
-        tags: ["react", "firebase", "redux"],
-
-        demo: "https://arjang-linkedin-clone.netlify.app/",
-        github: "https://github.com/arjang-psh/linkedin-react"
-    }, {
-        id: 5,
-        name: "Product Hunt",
-        description: "A website for share and discover new products.",
-        tags: ["html", "css", "javascript"],
-
-        demo: "https://netflix-clone-dummy.netlify.app/",
-        github: "https://github.com/arjang-psh/producthunt-project"
-
     },
-
 
 ] 
