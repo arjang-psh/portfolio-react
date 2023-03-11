298f15aedf6713227c2c2df25a83816340372a6c
arjang-psh <arji.pezeshki@gmail.com>
2023-03-10 16:30:08 -0800

    "about me page corrected"

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -164,7 +164,7 @@ const AboutPage = () => {
                     <Main>
                         I'm a front-end developer located in United States. I love to create simple yet beautiful websites with great user experience.
                         <br /> <br />
-                        I'm interested in the whole frontend stack Like trying new things and building great projects. I love to write cods and read books.
+                        I'm interested in the whole frontend stack Like trying new things and building great projects. I love to write code and read books.
                         <br /> <br />
                         I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.
                     </Main>
